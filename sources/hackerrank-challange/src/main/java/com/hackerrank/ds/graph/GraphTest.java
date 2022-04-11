96e5cb673131381bea8012285890e8fc93b6dcd6
Sachith Dickwella <sachith_prasanna@live.com>
2022-04-11 18:20:46 +0800

    Adding working basic BFS path-finding algorithm

--- sources/hackerrank-challange/src/main/java/com/hackerrank/ds/graph/GraphTest.java
+++ sources/hackerrank-challange/src/main/java/com/hackerrank/ds/graph/GraphTest.java
@@ -1,22 +1,26 @@
 package com.hackerrank.ds.graph;
 
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
-/**X
+/**
+ *
+ *
  * @author Sachith Dickwella
  */
+@SuppressWarnings("java:S106")
 public class GraphTest {
 
     public static void main(String[] args) {
         var data = new int[][]{
-                { 1,  2,  3,  4,  5},
-                { 6,  7,  8,  9, 10},
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
                 {11, 12, 13, 14, 15},
                 {16, 17, 18, 19, 20},
                 {21, 22, 23, 24, 25}
@@ -41,24 +45,48 @@ public class GraphTest {
         var dfs = depthFirstSearch(graph, 16);
         System.out.println(dfs);
 
-        int start = 16;
-        int target = 10;
-        var breadcrumbs = breadcrumbs(graph, start, target);
-        System.out.println(breadcrumbs);
-        var current = target;
-        var path = new LinkedList<Integer>();
-
-        var it = breadcrumbs.iterator();
-        while (current != start) {
-            path.add(current);
-            System.out.println(current);
-            current = breadcrumbs.get(current - 1);
-        }
-        path.add(start);
+        var path = path(graph, 5, 13);
         System.out.println(path);
     }
 
-    public static Set<Integer> breadthFirstSearch(Graph<Integer> graph, Integer root) {
+    private static Queue<Integer> path(Graph<Integer> graph, int start, int target) {
+        var breadcrumbs = breadcrumbs(graph, start, target);
+        System.out.println(breadcrumbs);
+        var current = target;
+        var path = new ArrayDeque<Integer>();
+
+        while (current != start) {
+            path.addFirst(current);
+            current = breadcrumbs.get(current);
+        }
+
+        path.addFirst(start);
+        return path;
+    }
+
+    private static Map<Integer, Integer> breadcrumbs(Graph<Integer> graph, int root, int target) {
+        var cameFrom = new HashMap<Integer, Integer>();
+        var frontier = new LinkedList<Integer>();
+
+        cameFrom.putIfAbsent(root, null);
+        frontier.offer(root);
+
+        while (!frontier.isEmpty()) {
+            var current = frontier.poll();
+
+            if (current == target) break;
+
+            for (Graph.Vertex<Integer> next : graph.getAdjacentVertex(current)) {
+                if (!cameFrom.containsKey(next.getLabel())) {
+                    frontier.offer(next.getLabel());
+                    cameFrom.putIfAbsent(next.getLabel(), current);
+                }
+            }
+        }
+        return cameFrom;
+    }
+
+    private static Set<Integer> breadthFirstSearch(Graph<Integer> graph, Integer root) {
         var visited = new LinkedHashSet<Integer>();
         var frontier = new LinkedList<Integer>();
         frontier.offer(root);
@@ -73,40 +101,18 @@ public class GraphTest {
                 }
             }
         }
-
         return visited;
-    }
-
-    public static List<Integer> breadcrumbs(Graph<Integer> graph, Integer root, Integer goal) {
-        var cameFrom = new LinkedList<Integer>();
-        var frontier = new LinkedList<Integer>();
-        cameFrom.add(root);
-        frontier.offer(root);
-
-        while(!frontier.isEmpty()) {
-            var vertex = frontier.poll();
-            if (vertex.equals(goal))
-                break;
-
-            for (Graph.Vertex<Integer> v : graph.getAdjacentVertex(vertex)) {
-                if (!cameFrom.contains(v.getLabel())) {
-                    frontier.offer(v.getLabel());
-                    cameFrom.add(v.getLabel());
-                }
-            }
-        }
-        return cameFrom;
     }
 
     /**
      *
      */
-    public static Set<Integer> depthFirstSearch(Graph<Integer> graph, Integer root) {
+    private static Set<Integer> depthFirstSearch(Graph<Integer> graph, Integer root) {
         var visited = new LinkedHashSet<Integer>();
         var frontier = new ArrayDeque<Integer>();
         frontier.push(root);
 
-        while(!frontier.isEmpty()) {
+        while (!frontier.isEmpty()) {
             var vertex = frontier.pop();
             for (Graph.Vertex<Integer> v : graph.getAdjacentVertex(vertex)) {
                 if (!visited.contains(v.getLabel())) {
